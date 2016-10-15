868d43bb7d6fef7ed2f7a20b9c8d163c2cc438c6
Shinya Sakemoto <sakebook@gmail.com>
2016-10-15 18:16:09 +0900

    Modify bottombar broadcast

--- app/src/main/java/com/sakebook/android/sample/customtabssample/receivers/BottombarBroadcastReceiver.java
+++ app/src/main/java/com/sakebook/android/sample/customtabssample/receivers/BottombarBroadcastReceiver.java
@@ -1,7 +1,89 @@
 package com.sakebook.android.sample.customtabssample.receivers;
 
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.sakebook.android.sample.customtabssample.R;
+
 /**
  * Created by sakemotoshinya on 2016/09/04.
  */
-public class BottombarBroadcastReceiver {
+public class BottombarBroadcastReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("CustomTabsSample", "BottombarBroadcastReceiver onReceive");
+        int clickedId = intent.getIntExtra(CustomTabsIntent.EXTRA_REMOTEVIEWS_CLICKED_ID, -1);
+        String url = intent.getDataString();
+        if (TextUtils.isEmpty(url)) {
+            Toast.makeText(context, "Failed share.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        switch (clickedId) {
+            case R.id.facebook:
+            case R.id.twitter:
+            case R.id.line:
+                share(context, url, clickedId);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void share(Context context, String text, int clickId) {
+        try {
+            switch (clickId) {
+                case R.id.facebook:
+                    shareFacebook(context, text);
+                    break;
+                case R.id.twitter:
+                    shareTwitter(context, text);
+                    break;
+                case R.id.line:
+                    shareLINE(context, text);
+                    break;
+            }
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(context, "Official client app not found.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void shareTwitter(Context context, String text) throws ActivityNotFoundException{
+        Intent shareIntent = new Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, text)
+                .setPackage("com.twitter.android")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                ;
+        context.startActivity(shareIntent);
+    }
+
+    /**
+     * Only work url
+     * reference: https://stackoverflow.com/questions/34618514/share-text-via-intent-on-facebook-without-using-facebook-sdk
+     * */
+    private void shareFacebook(Context context, String url) throws ActivityNotFoundException{
+        Intent shareIntent = new Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, url)
+                .setPackage("com.facebook.katana")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                ;
+        context.startActivity(shareIntent);
+    }
+
+    private void shareLINE(Context context, String text) throws ActivityNotFoundException{
+        Intent shareIntent = new Intent(Intent.ACTION_VIEW)
+                .setData(Uri.parse("line://msg/text/" + text))
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                ;
+        context.startActivity(shareIntent);
+    }
 }
--- app/src/main/java/com/sakebook/android/sample/customtabssample/utils/CustomTabsUtil.java
+++ app/src/main/java/com/sakebook/android/sample/customtabssample/utils/CustomTabsUtil.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.customtabs.CustomTabsIntent;
@@ -14,6 +15,9 @@ import android.text.TextUtils;
 
 import com.sakebook.android.sample.customtabssample.BuildConfig;
 import com.sakebook.android.sample.customtabssample.R;
+import com.sakebook.android.sample.customtabssample.receivers.BottombarBroadcastReceiver;
+import com.sakebook.android.sample.customtabssample.receivers.SessionBottombarBroadcastReceiver;
+import com.sakebook.android.sample.customtabssample.ui.CustomBottombar;
 
 import org.chromium.customtabsclient.shared.CustomTabsHelper;
 
@@ -22,74 +26,43 @@ import org.chromium.customtabsclient.shared.CustomTabsHelper;
  */
 public class CustomTabsUtil {
 
-    public static void launchCustomTabs(Activity activity, String url, @Nullable CustomTabsSession session) {
+    public static void launchCustomTabs(Activity activity, String url) {
         String packageName = CustomTabsHelper.getPackageNameToUse(activity);
         if (TextUtils.isEmpty(packageName)) {
-            // Supported Chrome is none.
-            // Fail back
-            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url + "?timestamp=" + SystemClock.currentThreadTimeMillis())));
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            return;
         }
-        CustomTabsIntent customTabsIntent;
-        if (session == null) {
-            customTabsIntent = new CustomTabsIntent.Builder().build();
-        } else {
-            customTabsIntent = new CustomTabsIntent.Builder(session).build();
-        }
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
         customTabsIntent.intent.setPackage(packageName);
-        customTabsIntent.intent.putExtra(Intent.EXTRA_REFERRER,
-                Uri.parse("android-app:" + "//" + BuildConfig.APPLICATION_ID + "/http/example.com"));
-//                Uri.parse(Intent.URI_ANDROID_APP_SCHEME + "//" + BuildConfig.APPLICATION_ID));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            customTabsIntent.intent.putExtra(Intent.EXTRA_REFERRER,
+                    Uri.parse(Intent.URI_ANDROID_APP_SCHEME + "//" + BuildConfig.APPLICATION_ID + "/http/example.com"));
+        } else {
+            customTabsIntent.intent.putExtra("android.intent.extra.REFERRER",
+                    Uri.parse("android-app:" + "//" + BuildConfig.APPLICATION_ID + "/http/example.com"));
+        }
         customTabsIntent.launchUrl(activity, Uri.parse(url));
     }
 
-    public static void launchCustomTabsWithBottombar(Activity activity, String url, @Nullable CustomTabsSession session) {
+    public static void launchCustomTabsWithDeprecatedBottombar(Activity activity, String url) {
         String packageName = CustomTabsHelper.getPackageNameToUse(activity);
         if (TextUtils.isEmpty(packageName)) {
-            // Supported Chrome is none.
-            // Fail back
-            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url + "?timestamp=" + SystemClock.currentThreadTimeMillis())));
-        }
-        CustomTabsIntent.Builder builder;
-        if (session == null) {
-            builder = new CustomTabsIntent.Builder();
-        } else {
-            builder = new CustomTabsIntent.Builder(session);
-        }
-        Intent shareIntent = new Intent();
-        shareIntent.setAction(Intent.ACTION_VIEW);
-        shareIntent.setData(Uri.parse("http://google.com"));
-        PendingIntent pendingIntent = PendingIntent.getActivity(activity, 100, shareIntent, 0);
-//        builder.addToolbarItem(1, BitmapFactory.decodeResource(activity.getResources(), R.mipmap.ic_launcher), "dec", pendingIntent);
-
-        int[] ids = {R.id.remote_text};
-//        builder.setSecondaryToolbarViews(CustomBottombarActivity.createRemoteViews(), ids, pendingIntent);
-
-
-        Bitmap icon = BitmapFactory.decodeResource(activity.getResources(), android.R.drawable.ic_menu_share);
-        builder.addToolbarItem(1, icon, "1", pendingIntent);
-        builder.setShowTitle(true);
-        builder.addDefaultShareMenuItem();
-
-        CustomTabsIntent customTabsIntent = builder.build();
-        CustomTabsHelper.addKeepAliveExtra(activity, customTabsIntent.intent);
-        customTabsIntent.intent.setPackage(packageName);
-        customTabsIntent.launchUrl(activity, Uri.parse(url));
-    }
-
-    public static void launchCustomTabsWithOldBottombar(Activity activity, String url) {
-        String packageName = CustomTabsHelper.getPackageNameToUse(activity);
-        if (TextUtils.isEmpty(packageName)) {
-            // Supported Chrome is none.
-            // Fail back
-            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url + "?timestamp=" + SystemClock.currentThreadTimeMillis())));
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
         }
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        addDeprecatedToolbar(builder, activity, url);
 
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.intent.setPackage(packageName);
+        customTabsIntent.launchUrl(activity, Uri.parse(url));
+    }
+
+    private static void addDeprecatedToolbar(CustomTabsIntent.Builder builder, Activity activity, String url) {
         Intent browseIntent = new Intent();
         browseIntent.setAction(Intent.ACTION_VIEW);
         browseIntent.setData(Uri.parse(url));
         PendingIntent pendingBrowseIntent = PendingIntent.getActivity(activity, 100, browseIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        Bitmap icon = BitmapFactory.decodeResource(activity.getResources(), android.R.drawable.ic_menu_view);
+        Bitmap icon = BitmapFactory.decodeResource(activity.getResources(), R.drawable.ic_open_in_browser_black);
         builder.addToolbarItem(1, icon, "view", pendingBrowseIntent);
 
         Intent shareIntent = new Intent();
@@ -97,12 +70,57 @@ public class CustomTabsUtil {
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, url);
         PendingIntent pendingShareIntent = PendingIntent.getActivity(activity, 101, shareIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        Bitmap icons = BitmapFactory.decodeResource(activity.getResources(), android.R.drawable.ic_menu_share);
+        Bitmap icons = BitmapFactory.decodeResource(activity.getResources(), R.drawable.ic_share_black);
         builder.addToolbarItem(2, icons, "share", pendingShareIntent);
+    }
 
+
+    public static void launchCustomTabsWithBottombar(Activity activity, String url) {
+        String packageName = CustomTabsHelper.getPackageNameToUse(activity);
+        if (TextUtils.isEmpty(packageName)) {
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        }
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        addBottombar(builder, activity, url);
         CustomTabsIntent customTabsIntent = builder.build();
+        CustomTabsHelper.addKeepAliveExtra(activity, customTabsIntent.intent);
         customTabsIntent.intent.setPackage(packageName);
         customTabsIntent.launchUrl(activity, Uri.parse(url));
     }
 
+    private static void addBottombar(CustomTabsIntent.Builder builder, Activity activity, String url) {
+        Intent broadcastIntent = new Intent(activity, BottombarBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(activity, 110, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        int[] ids = {R.id.twitter, R.id.line, R.id.facebook};
+        builder.setSecondaryToolbarViews(CustomBottombar.createBottombar(), ids, pendingIntent);
+        builder.setShowTitle(true);
+    }
+
+    public static void launchCustomTabsWithSessionBottombar(Activity activity, String url, @Nullable CustomTabsSession session) {
+        String packageName = CustomTabsHelper.getPackageNameToUse(activity);
+        if (TextUtils.isEmpty(packageName)) {
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        }
+        CustomTabsIntent.Builder builder;
+        if (session == null) {
+            builder = new CustomTabsIntent.Builder();
+        } else {
+            builder = new CustomTabsIntent.Builder(session);
+        }
+        addSessionBottombar(builder, activity, url);
+
+        CustomTabsIntent customTabsIntent = builder.build();
+        CustomTabsHelper.addKeepAliveExtra(activity, customTabsIntent.intent);
+        customTabsIntent.intent.setPackage(packageName);
+        customTabsIntent.launchUrl(activity, Uri.parse(url));
+    }
+
+    private static void addSessionBottombar(CustomTabsIntent.Builder builder, Activity activity, String url) {
+        Intent broadcastIntent = new Intent(activity, SessionBottombarBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(activity, 120, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        int[] ids = {R.id.image_favorite};
+        builder.setSecondaryToolbarViews(CustomBottombar.createSessionBottombar(), ids, pendingIntent);
+        builder.setShowTitle(true);
+        builder.addDefaultShareMenuItem();
+    }
 }
