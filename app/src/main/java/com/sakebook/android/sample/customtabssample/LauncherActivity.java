7cbc81f8321d1aa6834eac007ef24bb0cc68005e
Shinya Sakemoto <sakebook@gmail.com>
2017-02-22 23:29:45 +0900

    Add custom request

--- app/src/main/java/com/sakebook/android/sample/customtabssample/LauncherActivity.java
+++ app/src/main/java/com/sakebook/android/sample/customtabssample/LauncherActivity.java
@@ -1,9 +1,7 @@
 package com.sakebook.android.sample.customtabssample;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,19 +11,18 @@ import android.view.ViewGroup;
 
 import com.sakebook.android.sample.customtabssample.ui.BottombarActivity;
 import com.sakebook.android.sample.customtabssample.ui.CustomAnimationActivity;
+import com.sakebook.android.sample.customtabssample.ui.CustomRequestActivity;
 import com.sakebook.android.sample.customtabssample.ui.CustomToolbarActivity;
 import com.sakebook.android.sample.customtabssample.ui.SessionBottombarActivity;
 import com.sakebook.android.sample.customtabssample.ui.DeprecatedBottombarActivity;
 import com.sakebook.android.sample.customtabssample.ui.PrefetchActivity;
-import com.sakebook.android.sample.customtabssample.utils.CustomTabsUtil;
 
 import org.chromium.customtabsclient.shared.CustomTabsHelper;
 
 public class LauncherActivity extends AppCompatActivity {
 
-    private final static String URL = "https://developer.android.com/index.html";
-//    private final static String URL = "http://cgi.b4iine.net/env/";
-
+    private final static String HOME_URL = "https://developer.android.com/index.html";
+    private final static String CUSTOM_URL = "https://httpbin.org/headers";
 
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
@@ -33,28 +30,31 @@ public class LauncherActivity extends AppCompatActivity {
             Intent intent;
             switch (view.getId()) {
                 case R.id.launch_button:
-                    launchCustomTabs(URL);
+                    launchCustomTabs(HOME_URL);
                     return;
                 case R.id.custom_toolbar_button:
-                    intent = CustomToolbarActivity.createIntent(LauncherActivity.this, URL);
+                    intent = CustomToolbarActivity.createIntent(LauncherActivity.this, HOME_URL);
                     break;
                 case R.id.animation_button:
-                    intent = CustomAnimationActivity.createIntent(LauncherActivity.this, URL);
+                    intent = CustomAnimationActivity.createIntent(LauncherActivity.this, HOME_URL);
+                    break;
+                case R.id.custom_request_button:
+                    intent = CustomRequestActivity.createIntent(LauncherActivity.this, CUSTOM_URL);
                     break;
                 case R.id.prefetch_button:
-                    intent = PrefetchActivity.createIntent(LauncherActivity.this, URL);
+                    intent = PrefetchActivity.createIntent(LauncherActivity.this, HOME_URL);
                     break;
                 case R.id.deprecated_bottom_bar_button:
-                    intent = DeprecatedBottombarActivity.createIntent(LauncherActivity.this, URL);
+                    intent = DeprecatedBottombarActivity.createIntent(LauncherActivity.this, HOME_URL);
                     break;
                 case R.id.bottombar_button:
-                    intent = BottombarActivity.createIntent(LauncherActivity.this, URL);
+                    intent = BottombarActivity.createIntent(LauncherActivity.this, HOME_URL);
                     break;
                 case R.id.session_bottom_bar_button:
                     intent = SessionBottombarActivity.createIntent(LauncherActivity.this);
                     break;
                 default:
-                    intent = CustomToolbarActivity.createIntent(LauncherActivity.this, URL);
+                    intent = CustomToolbarActivity.createIntent(LauncherActivity.this, HOME_URL);
                     break;
             }
             startActivity(intent);
---
+++ app/src/main/java/com/sakebook/android/sample/customtabssample/ui/CustomRequestActivity.java
@@ -0,0 +1,97 @@
+package com.sakebook.android.sample.customtabssample.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Browser;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.sakebook.android.sample.customtabssample.BuildConfig;
+import com.sakebook.android.sample.customtabssample.R;
+import com.sakebook.android.sample.customtabssample.utils.CustomTabsUtil;
+
+import org.chromium.customtabsclient.shared.CustomTabsHelper;
+
+public class CustomRequestActivity extends AppCompatActivity {
+
+    private final static String URL_ARGS = "url";
+    private String url = "";
+
+    public static Intent createIntent(Context context, String url) {
+        Intent intent = new Intent(context, CustomRequestActivity.class);
+        intent.putExtra(URL_ARGS, url);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_request);
+        url = getIntent().getStringExtra(URL_ARGS);
+
+        findViewById(R.id.button_request_header).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCustomTabsWithHeader(url);
+            }
+        });
+        findViewById(R.id.button_request_referrer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCustomTabsWithReferrer(url);
+            }
+        });
+        findViewById(R.id.button_request_normal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCustomTabs(url);
+            }
+        });
+    }
+
+    private void launchCustomTabsWithHeader(String url) {
+        String packageName = CustomTabsHelper.getPackageNameToUse(this);
+        if (TextUtils.isEmpty(packageName)) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            return;
+        }
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+        customTabsIntent.intent.setPackage(packageName);
+        Bundle headers = new Bundle();
+        headers.putString("header1", "value1");
+        headers.putString("header2", "value2");
+        customTabsIntent.intent.putExtra(Browser.EXTRA_HEADERS, headers);
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
+
+    private void launchCustomTabsWithReferrer(String url) {
+        String packageName = CustomTabsHelper.getPackageNameToUse(this);
+        if (TextUtils.isEmpty(packageName)) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            return;
+        }
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+        customTabsIntent.intent.setPackage(packageName);
+        customTabsIntent.intent.putExtra(Intent.EXTRA_REFERRER,
+                Uri.parse("android-app:" + "//" + BuildConfig.APPLICATION_ID));
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
+
+    private void launchCustomTabs(String url) {
+        String packageName = CustomTabsHelper.getPackageNameToUse(this);
+        if (TextUtils.isEmpty(packageName)) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            return;
+        }
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+        customTabsIntent.intent.setPackage(packageName);
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
+}
